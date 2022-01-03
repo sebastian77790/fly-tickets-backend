46c8cef09c5b16498d1b10ed2fb5cf727efc8e2d
sebastian77790 <sebastian77790@gmail.com>
2022-01-03 17:37:20 -0500

    Add files via upload

---
+++ Controllers/FlightsController.cs
@@ -0,0 +1,34 @@
+﻿using FlyTickets.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlyTickets.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FlightsController : ControllerBase
+    {
+
+        [HttpGet(Name = "GetValues")]
+        public ActionResult Get()
+        {
+            using (var db = new testContext())
+            {
+                var Parameter = new SqlParameter("@parameter", 4);
+                //var lst = db.FlightsInfos
+                //        .SqlQuery<dynamic>($"EXECUTE dbo.GetMostPopularBlogsForUser {Parameter}")
+                //.ToList();
+                var lst = "";
+                return Ok(lst);
+            }
+
+        }
+
+    }
+}
+
+
+
+
